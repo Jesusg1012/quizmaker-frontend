211fdf712f69a3ace07890aab3eacbf775a27c3d
Jesus <jesus.jg.medina@gmail.com>
2019-03-05 16:20:07 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const mainDiv = document.querySelector("div")
+
+function getAll(){
+  return fetch("http://localhost:3000/tests")
+}
+
+getAll()
+.then(res => res.json())
+.then(json => {
+  json.forEach(printOnScreen)
+})
+
+function printOnScreen(test){
+  mainDiv.innerHTML += `
+  <div> ${test.title}</div>
+  <div> ${test.description}</div>`
+}
